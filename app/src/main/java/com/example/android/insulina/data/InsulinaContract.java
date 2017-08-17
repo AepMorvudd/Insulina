c3f1f78e6d6d9239ceebe28497b4a7b2746e8503
AepMorvudd <AepMorvudd@users.noreply.github.com>
2017-08-17 20:07:54 +0200

    Database Contract Added
    Added a Contract for Database, also clean build upload

---
+++ app/src/main/java/com/example/android/insulina/data/InsulinaContract.java
@@ -0,0 +1,42 @@
+package com.example.android.insulina.data;
+
+import android.provider.BaseColumns;
+
+public final class InsulinaContract {
+
+    private InsulinaContract() {}
+
+    public static class InsulinaEntry implements BaseColumns {
+        //Table name
+        public static final String TABLE_NAME = "insulina";
+
+        //Column names
+
+        // Column ID
+        public static final String _ID = BaseColumns._ID;
+
+        /*
+        Column name
+        Takes String, can not be left empty
+         */
+        public static final String COLUMN_INSULINA_NAME = "nazwa";
+
+        /*
+        Ammount of Insuline intake
+        Integer, can not be left empty
+         */
+        public static final String COLUMN_INSULINA_INTAKE = "jednostki";
+
+        /*
+        Short description
+        String, optional entry
+         */
+        public static final String COLUMN_INSULINA_DESCRIPTION = "opis";
+
+        /*
+        Ammount of glucose level 2h after eating
+        Integer, optional entry
+         */
+        public static final String COLUMN_INSULINA_GLUCOSE_2H_LATER = "cukier";
+    }
+}
